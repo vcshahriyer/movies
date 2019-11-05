941f8eb76b12d2501c832bb3bb54d6b8e69f040c
vcshahriyer <shahriyer99@gmail.com>
2019-11-05 08:22:29 +0600

    Routing implemented

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 import React, { Component } from "react";
 import Movies from "./components/movies";
 import "./App.css";
+import { Route, Redirect, Switch } from "react-router-dom";
+import NavBar from "./components/navBar";
+import Customers from "./components/customers";
+import Rentals from "./components/rentals";
+import NotFound from "./components/notFound";
+import MovieForm from "./components/movieForm";
 
 class App extends Component {
   render() {
     return (
-      <main className="container">
-        <Movies />
-      </main>
+      <React.Fragment>
+        <NavBar />
+        <main className="container">
+          <Switch>
+            <Route path="/movies/:id" component={MovieForm}></Route>
+            <Route path="/movies" component={Movies}></Route>
+            <Route path="/customers" component={Customers}></Route>
+            <Route path="/rentals" component={Rentals}></Route>
+            <Route path="/not-found" component={NotFound}></Route>
+            <Redirect from="/" exact to="/movies" />
+            <Redirect to="/not-found" />
+          </Switch>
+        </main>
+      </React.Fragment>
     );
   }
 }
--- src/components/moviesTable.jsx
+++ src/components/moviesTable.jsx
@@ -1,10 +1,14 @@
 import React, { Component } from "react";
 import Like from "./common/like";
 import Table from "./common/table";
-
+import { Link } from "react-router-dom";
 class MoviesTable extends Component {
   columns = [
-    { path: "title", lable: "Title" },
+    {
+      path: "title",
+      lable: "Title",
+      content: movie => <Link to={`/movies/${movie._id}`}>{movie.title}</Link>
+    },
     { path: "genre.name", lable: "Genre" },
     { path: "numberInStock", lable: "Stock" },
     { path: "dailyRentalRate", lable: "Rate" },
